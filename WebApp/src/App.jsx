fb2c420413e24918c1690274a48d9caee8a498e4
StormBlessed27 <hjballent@gmail.com>
2025-01-11 18:42:50 -0500

    First Frontend Version

---
+++ WebApp/src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+import { Board } from './Board'
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='flex justify-center items-center bg-slate-700 h-full'>
+      <Board/>
+    </main>
+  )
+}
+
+export default App
