2760aa30d681dbd6b480659e1ca24f2df386e057
StormBlessed27 <hjballent@gmail.com>
2025-01-16 17:48:36 -0500

    adding the auth router

--- Api/src/Routes/index.js
+++ Api/src/Routes/index.js
@@ -1,8 +1,10 @@
+import { authRouter } from './authRouter.js'
 import { messageRouter } from './messageRouter.js'
 import { userRouter } from './usersRouter.js'
 import { Router } from 'express'
 
 export const router = Router()
 
+router.use('/auth', authRouter)
 router.use('/users', userRouter)
 router.use('/messages', messageRouter)
