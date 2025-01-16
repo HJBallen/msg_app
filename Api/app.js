9ac032a9157f0bcc95054c480538481e96ac2b3d
StormBlessed27 <hjballent@gmail.com>
2025-01-16 17:45:16 -0500

    Using the cookieParser middleware

--- Api/app.js
+++ Api/app.js
@@ -4,11 +4,13 @@ import { router } from './src/Routes/index.js'
 import cors from 'cors'
 
 import { PORT } from './config.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use('/api/v1/', router)
 
 app.get('/', (req, res) => {
