f28377dc9faeff22954d585c73504ef0e4b50256
StormBlessed27 <hjballent@gmail.com>
2025-01-25 16:17:39 -0500

    First version of the register component

--- WebApp/src/Components/Register/index.jsx
+++ WebApp/src/Components/Register/index.jsx
@@ -1,18 +1,70 @@
+import { useContext, useState } from 'react'
+import { loginContext } from '../../context/loginContext'
+import { RegisterNormalView } from './RegisterNormalView'
+import { RegisterErrView } from './RegisterErrView'
+import { config } from '../../config'
+
 export function Register() {
-  const register = ()=>{}
+  const [errMsg, setErrMsg] = useState('')
+  const [registerErr, setRegisterErr] = useState(false)
+  const {setUsername, setUserImg, setIsLogin} = useContext(loginContext)
+  const registerSubmit = async function(data) {
+    try {
+      const fetchOptions = {
+        method:"POST", 
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body: JSON.stringify(data)
+      }
+      const res  = await fetch(config.BASE_API_URL+"/users/create",fetchOptions)
+      const jsonRes = await res.json()
+
+      if (jsonRes.statusCode) {
+        setErrMsg(jsonRes.message)
+        setRegisterErr(true)
+      }
+
+    } catch (error) {
+      console.log(error);
+      return false
+    }
+  }
+
+  const passwordConfirm = (e)=>{
+    e.preventDefault()
+    const password = e.target.previousSibling.value
+    const passwordConfirm = e.target.value
+    if (password !== passwordConfirm){
+      e.target.style.border = '2px solid red'
+    }else{
+      e.target.style.border = '2px solid green'
+    }
+  
+      
+  }
+
+  const register = async (e)=>{
+    e.preventDefault()
+    const newUser = {username: e.target.username.value, password: e.target.password.value, user_img:e.target.user_img.value}
+    console.log(newUser);
+    
+    const res = await registerSubmit(newUser)
+
+    if (!res) return false
+
+    setUsername(res.username)
+    setUserImg(res.user_img??"")
+    setIsLogin(true)
+  }
 
   return (
-  <>
-    <div className='flex flex-col bg-slate-400/55 0 w-1/3 h-1/3 justify-center items-center rounded-xl shadow-lg backdrop-blur-xl'>
-      <h1 className='text-purple-600/80 text-3xl mb-4 font-semibold'>Login</h1>
-      <form onSubmit={register} className='flex flex-col items-center gap-y-3'>
-        <input type="text" placeholder='Username...' className='h-8 font-semibold rounded-lg items-center transition-transform hover:scale-105'/>
-        <input placeholder='Password...' className='h-8 font-semibold rounded-lg items-center transition-transform hover:scale-105 hover:border-2 border-yellow-600'/>
-        <input placeholder='Confirm Password...' className='h-8 font-semibold rounded-lg items-center transition-transform hover:scale-105 hover:border-2 border-yellow-600'/>
-        <input placeholder='User img' type='fyle' className='h-8 font-semibold rounded-lg items-center transition-transform hover:scale-105 hover:border-2 border-yellow-600'/>
-        <button className='rounded-full text-white font-semibold bg-purple-600/90 w-16 h-8 transition-transform hover:scale-110'>Enter</button>
-      </form>
-    </div>
-  </>
+    <>
+      {!registerErr 
+        ? <RegisterNormalView register = {register} passwordConfirm={passwordConfirm}/> 
+        : <RegisterErrView register={register} passwordConfirm={passwordConfirm} errMsg={errMsg}/>
+      }
+    </>
+    
   )
 }
