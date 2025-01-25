ce85dd61c34d24aa0e53b5f9ba6a95c547f83077
StormBlessed27 <hjballent@gmail.com>
2025-01-25 16:16:57 -0500

    Added: RegisterErrView to control de error responses

---
+++ WebApp/src/Components/Register/RegisterErrView.jsx
@@ -0,0 +1,17 @@
+export function RegisterErrView({register, passwordConfirm, errMsg}) {
+  return(
+    <div className='flex flex-col bg-slate-400/55 0 w-1/3 h-1/3 justify-center items-center rounded-xl shadow-lg backdrop-blur-xl'>
+      <a href="/" className='absolute top-8 left-10'>◀</a>
+      <h1 className='text-purple-600/80 text-3xl mb-4 font-semibold'>Login</h1>
+      <form onSubmit={register} className='flex flex-col items-center gap-y-3' >
+        <input  name='username' type="text" placeholder='Username...' className='h-8 font-semibold rounded-lg items-center transition-transform hover:scale-105' required/>
+        <input placeholder='Password...' name='password' type='password' minLength='6'className='h-8 font-semibold rounded-lg items-center transition-transform hover:scale-105 hover:border-2 border-yellow-600' required/>
+        <input placeholder='Confirm Password...' onKeyUp={passwordConfirm} name='password_confirm' type='password' className='h-8 font-semibold rounded-lg items-center transition-transform hover:scale-105 hover:border-2 border-yellow-600' required/>
+        <input placeholder='User img' type='text' name="user_img" className='h-8 font-semibold rounded-lg items-center transition-transform hover:scale-105 hover:border-2 border-yellow-600'/>
+        <label className='bg-red-400 w-40 text-center text-xs font-semibold rounded-md'>{errMsg}</label>
+        <button className='rounded-full text-white font-semibold bg-purple-600/90 w-16 h-8 transition-transform hover:scale-110'>Enter</button>
+      </form>
+    </div>
+  )
+}
+
