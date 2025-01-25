10eacd441e8de8e1015c5c629e935ae181d4752a
StormBlessed27 <hjballent@gmail.com>
2025-01-25 14:48:42 -0500

    Main component with the page routes

---
+++ WebApp/src/Components/Main/index.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+
+import { Routes, Route, useNavigate } from "react-router"
+import { Board } from '../Board'
+import { Login } from '../Login'
+import { Register } from '../Register'
+
+export function MainApp({isLogin}) {
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    console.log('se activo el efecto');
+    
+      if (isLogin) {
+        navigate('/board')
+      }
+    },[isLogin, navigate])
+
+  return (
+  <>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/board' element={<Board/>}/>
+    </Routes>
+  </>
+  )
+
+}
